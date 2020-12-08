60f66240e3b5ea2afdf5ff9eeebb370c165654c5
Viki7sh <62552986+Viki7sh@users.noreply.github.com>
2020-12-08 21:51:36 +0300

    Add files via upload

---
+++ Lab 3/Service2/Options.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    public class Options
+    {
+        public DirectoryOptions DirectoryOptions { get; set; }
+        public ArchiveOptions ArchiveOptions { get; set; }
+    }
+
+    public class DirectoryOptions
+    {
+        public string SourceDirectory { get; set; }
+        public string TargetDirectory { get; set; }
+    }
+
+    public class ArchiveOptions
+    {
+        public string ArchiveName { get; set; }
+    }
+}
